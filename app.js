2e662192396b959e94d5f84d5b0636078941aa6f
Ed McCormic <ed@edmccormic.com>
2021-02-11 12:19:26 -0500

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+var path = __dirname + '/public/';
+const router = express.Router();
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+app.use("/",router);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
